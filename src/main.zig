b3a729ff51b311799bce2c85dbc72669b3fd1af5
echohumm <echo0humm@gmail.com>
2026-05-13 21:32:16 -0600

    add some more helpers, do some cleanup

--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,5 @@ const arc = @import("root.zig");
 
 pub fn main() !void {
     var val = try arc.Arc(u32, std.heap.DebugAllocator(.{})).init(0, std.heap.DebugAllocator(.{}){});
-    try val.deinit();
+    defer val.deinit();
 }
--- src/root.zig
+++ src/root.zig
@@ -1,28 +1,50 @@
 const std = @import("std");
 
+/// check if `T` has method `name` with signature `Sig`
 fn hasMethod(comptime T: type, comptime name: []const u8, comptime Sig: type) bool {
     if (!@hasDecl(T, name)) return false;
     return @TypeOf(@field(T, name)) == Sig;
 }
 
+/// check if `T` has method `name` with either signature in `sigs`
+fn hasAnyMethod(comptime T: type, comptime name: []const u8, comptime sigs: [2]type) bool {
+    switch (@typeInfo(@TypeOf(T))) {
+        .@"struct", .@"enum", .@"union", .@"opaque" => {},
+        else => { return false; }
+    }
+    if (!@hasDecl(T, name)) return false;
+    inline for (sigs) |Sig| {
+        if (@FieldType(T, name) == Sig) return true;
+    }
+    return false;
+}
+
+/// attempts to destruct `val` of type `T`
+fn tryDeinit(comptime T: type, val: T) void {
+    if (comptime hasAnyMethod(T, "deinit", .{ fn (*T) void, fn (T) void })) {
+        val.deinit();
+    }
+}
+
+/// create monomorphized shared Inner type for Arc and Weak
+fn Inner(comptime T: type, comptime A: type) type {
+    return struct { strong: std.atomic.Value(usize), weak: std.atomic.Value(usize), alloc: A, data: T };
+}
+
 pub fn Arc(comptime T: type, comptime A: type) type {
     return struct {
         const Self = @This();
+        // TODO: better name for shorthand
+        const Data = Inner(T, A);
 
-        const Inner = struct {
-            strong: std.atomic.Value(usize),
-            weak: std.atomic.Value(usize),
-            alloc: A,
-            data: T
-        };
-        inner: *Inner,
+        inner: *Data,
 
         pub fn init(data: T, alloc: A) !Self {
             var a = alloc;
-            const inner: *Inner = if (comptime hasMethod(A, "allocator", fn(*A) std.mem.Allocator)) blk: {
-                break :blk try a.allocator().create(Inner);
+            const inner: *Data = if (comptime hasMethod(A, "allocator", fn (*A) std.mem.Allocator)) blk: {
+                break :blk try a.allocator().create(Data);
             } else if (comptime A == std.mem.Allocator) blk: {
-                break :blk try a.create(Inner);
+                break :blk try a.create(Data);
             } else {
                 @compileError(std.fmt.comptimePrint(
                     "type {s} is incompatible with Arc",
@@ -35,12 +57,11 @@ pub fn Arc(comptime T: type, comptime A: type) type {
                 .alloc = alloc,
                 .data = data,
             };
-            return Self { .inner = inner };
+            return Self{ .inner = inner };
         }
 
-        pub fn deinit(self: Self) !void {
-            // TODO
-            _ = self;
+        pub fn deinit(self: Self) void {
+            @compileError("unimplemented");
         }
 
         pub fn clone(self: Self) !Self {
