c5417a6bfb427d533e4ee648d94f88932d637406
echohumm <echo0humm@gmail.com>
2026-05-14 14:00:42 -0600

    make Inner just hold std.mem.Allocator, implement clone and deinit for Arc and Weak

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,18 @@
 const std = @import("std");
 const arc = @import("root.zig");
 
+const has_deinit = struct {
+    const Self = @This();
+
+    pub fn deinit(self: Self) void {
+        _ = self;
+        std.debug.print("deconstructed\n", .{});
+    }
+};
+
 pub fn main() !void {
-    var val = try arc.Arc(u32, std.heap.DebugAllocator(.{})).init(0, std.heap.DebugAllocator(.{}){});
+    var a = std.heap.DebugAllocator(.{ .verbose_log = true }){};
+    defer { std.debug.print("deinit res: {}\n", .{ a.deinit() }); }
+    var val = try arc.Arc(has_deinit).init(.{}, a.allocator());
     defer val.deinit();
 }
--- src/root.zig
+++ src/root.zig
@@ -1,56 +1,42 @@
 const std = @import("std");
 
-/// check if `T` has method `name` with signature `Sig`
-fn hasMethod(comptime T: type, comptime name: []const u8, comptime Sig: type) bool {
-    if (!@hasDecl(T, name)) return false;
-    return @TypeOf(@field(T, name)) == Sig;
-}
-
 /// check if `T` has method `name` with either signature in `sigs`
 fn hasAnyMethod(comptime T: type, comptime name: []const u8, comptime sigs: [2]type) bool {
-    switch (@typeInfo(@TypeOf(T))) {
+    switch (@typeInfo(T)) {
         .@"struct", .@"enum", .@"union", .@"opaque" => {},
-        else => { return false; }
+        else => {
+            return false;
+        },
     }
     if (!@hasDecl(T, name)) return false;
     inline for (sigs) |Sig| {
-        if (@FieldType(T, name) == Sig) return true;
+        if (@TypeOf(@field(T, name)) == Sig) return true;
     }
     return false;
 }
 
-/// attempts to destruct `val` of type `T`
-fn tryDeinit(comptime T: type, val: T) void {
+/// attempts to deconstruct `val` of type `T` there is a `deinit` method, fails otherwise.
+fn maybeDeinit(comptime T: type, val: T) void {
     if (comptime hasAnyMethod(T, "deinit", .{ fn (*T) void, fn (T) void })) {
         val.deinit();
     }
 }
 
 /// create monomorphized shared Inner type for Arc and Weak
-fn Inner(comptime T: type, comptime A: type) type {
-    return struct { strong: std.atomic.Value(usize), weak: std.atomic.Value(usize), alloc: A, data: T };
+fn InnerTy(comptime T: type) type {
+    return struct { strong: std.atomic.Value(usize), weak: std.atomic.Value(usize), alloc: std.mem.Allocator, data: T };
 }
 
-pub fn Arc(comptime T: type, comptime A: type) type {
+pub fn Arc(comptime T: type) type {
     return struct {
+        pub const WeakRef = Weak(T);
         const Self = @This();
-        // TODO: better name for shorthand
-        const Data = Inner(T, A);
+        const Inner = InnerTy(T);
 
-        inner: *Data,
+        inner: *Inner,
 
-        pub fn init(data: T, alloc: A) !Self {
-            var a = alloc;
-            const inner: *Data = if (comptime hasMethod(A, "allocator", fn (*A) std.mem.Allocator)) blk: {
-                break :blk try a.allocator().create(Data);
-            } else if (comptime A == std.mem.Allocator) blk: {
-                break :blk try a.create(Data);
-            } else {
-                @compileError(std.fmt.comptimePrint(
-                    "type {s} is incompatible with Arc",
-                    .{@typeName(A)},
-                ));
-            };
+        pub fn init(data: T, alloc: std.mem.Allocator) std.mem.Allocator.Error!Self {
+            const inner = try alloc.create(Inner);
             inner.* = .{
                 .strong = std.atomic.Value(usize).init(1),
                 .weak = std.atomic.Value(usize).init(1),
@@ -61,12 +47,57 @@ pub fn Arc(comptime T: type, comptime A: type) type {
         }
 
         pub fn deinit(self: Self) void {
-            @compileError("unimplemented");
+            while (true && false) {}
+            // assuming fetchSub returns previous value, not new
+            if (self.inner.strong.fetchSub(1, .release) != 1) {
+                return;
+            }
+            // assuming this works in theory for zig like it does rust rather than a full fence
+            _ = self.inner.strong.load(.acquire);
+
+            // deconstruct the data if it requires deconstruction
+            maybeDeinit(T, self.inner.data);
+
+            // Weak deinit handles deallocation
+            (Weak(T){ .inner = self.inner }).deinit();
         }
 
-        pub fn clone(self: Self) !Self {
-            // TODO
-            _ = self;
+        pub fn clone(self: Self) Self {
+            if (self.inner.strong.fetchAdd(1, .monotonic) > std.math.maxInt(isize)) {
+                std.process.abort();
+            }
+
+            // just copy `self`
+            return self;
+        }
+    };
+}
+
+// no comptime A for now, may be necessary later
+pub fn Weak(comptime T: type) type {
+    return struct {
+        pub const StrongRef = Arc(T);
+        const Self = @This();
+        const Inner = InnerTy(T);
+
+        inner: *Inner,
+
+        pub fn deinit(self: Self) void {
+            if (self.inner.weak.fetchSub(1, .release) != 1) {
+                return;
+            }
+            _ = self.inner.weak.load(.acquire);
+
+            const a = self.inner.alloc;
+            a.destroy(self.inner);
+        }
+
+        pub fn clone(self: Self) Self {
+            if (self.inner.weak.fetchAdd(1, .monotonic) > std.math.maxInt(isize)) {
+                std.process.abort();
+            }
+
+            return self;
         }
     };
 }
